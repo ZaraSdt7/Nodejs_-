ba064ff647b1ad61fa5ddb014724e4b86e2d80a4
zahra <zahra.st7373@gmail.com>
2024-03-05 14:43:16 +0330

    ok

--- index.js
+++ index.js
@@ -1,28 +1,55 @@
-const http = require("http")
-const port = 3000;
+// const http = require("http")
+// const port = 3000;
 
-const server = http.createServer((req,res)=>{
-    const {method , url} = req;
+// const server = http.createServer((req,res)=>{
+//     const {method , url} = req;
 
-    //handler
-    if(method === "GET" && url === '/'){
-        res.write('Hello world')
-        res.end();
-    }
-    else if(method === "POST" && url === '/'){
-        const body = '';
-        req.on('data',(chunk)=>{
-            body += chunk;
-        })
-        res.on('end',()=>{
-            res.write(body)
+//     //handler
+//     if(method === "GET" && url === '/'){
+//         res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end('Hello World!');
+//     }
+//     else if(method === "POST" && url === '/'){
+//         const body = '';
+//         req.on('data',(chunk)=>{
+//             body = chunk.toString();
+//         })
+//         res.on('end',()=>{
+//             res.writeHead(200, { 'Content-Type': 'text/plain' });
+//             res.end(body);
+//         })
+//     }
+//     else{
+//         res.writeHead(404, { 'Content-Type': 'text/plain' });
+//         res.end('Not Found');
+//     }
+// })
+// server.listen(port,()=>{
+//     console.log(`server is listening on ${port}`);
+// })
+
+const http = require("http");
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+
+    res.setHeader("Access-Control-Allow-Origin", "*")
+
+    const parseddata = url.parse(req.url, true)
+
+    const reqUrl = parseddata.pathname
+
+    if (req.method == "GET") {
+        if (reqUrl == "/") {
+            res.write("Hello, data sent\n" +  JSON.stringify(parseddata.query))
             res.end()
-        })
-    }
-    else{
-        res.end('Bad Request')
+        }
+    } else if (req.method == "POST") {
+        if (reqUrl == "/hello") {
+            res.write("hello world")
+            res.end()
+        }
     }
 })
-server.listen(port,()=>{
-    console.log(`server is listening on ${port}`);
-})
+
+server.listen(3000)
