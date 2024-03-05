cc2e11857b7960544a95a76c0812c5596367089d
zahra <zahra.st7373@gmail.com>
2024-03-05 15:23:56 +0330

    ok

--- index.js
+++ index.js
@@ -1,55 +1,29 @@
-// const http = require("http")
-// const port = 3000;
+const http = require("http")
+const port = 3000;
 
-// const server = http.createServer((req,res)=>{
-//     const {method , url} = req;
+const server = http.createServer((req,res)=>{
+    const {method , url} = req;
 
-//     //handler
-//     if(method === "GET" && url === '/'){
-//         res.writeHead(200, { 'Content-Type': 'text/plain' });
-//     res.end('Hello World!');
-//     }
-//     else if(method === "POST" && url === '/'){
-//         const body = '';
-//         req.on('data',(chunk)=>{
-//             body = chunk.toString();
-//         })
-//         res.on('end',()=>{
-//             res.writeHead(200, { 'Content-Type': 'text/plain' });
-//             res.end(body);
-//         })
-//     }
-//     else{
-//         res.writeHead(404, { 'Content-Type': 'text/plain' });
-//         res.end('Not Found');
-//     }
-// })
-// server.listen(port,()=>{
-//     console.log(`server is listening on ${port}`);
-// })
-
-const http = require("http");
-const url = require("url");
-
-const server = http.createServer((req, res) => {
-
-    res.setHeader("Access-Control-Allow-Origin", "*")
-
-    const parseddata = url.parse(req.url, true)
-
-    const reqUrl = parseddata.pathname
-
-    if (req.method == "GET") {
-        if (reqUrl == "/") {
-            res.write("Hello, data sent\n" +  JSON.stringify(parseddata.query))
-            res.end()
-        }
-    } else if (req.method == "POST") {
-        if (reqUrl == "/hello") {
-            res.write("hello world")
-            res.end()
-        }
+    //handler
+    if(method === "GET" && url === '/'){
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello, world!');
+    }
+    else if(method === "POST" && url === '/'){
+        let body = '';
+        req.on('data',(chunk)=>{
+            body += chunk;
+        })
+        req.on('end',()=>{
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(body);
+        })
+    }
+    else{
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
     }
 })
-
-server.listen(3000)
+server.listen(port,()=>{
+    console.log(`server is listening on ${port}`);
+})
