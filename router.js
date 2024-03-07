526d556826f388c92b724822cd8a4f52909b9943
zahra <zahra.st7373@gmail.com>
2024-03-07 14:21:09 +0330

    ok

--- router.js
+++ router.js
@@ -1,86 +1,19 @@
-// const http = require("http");
-// let port = 5000;
 
-// const router ={
-//     '/':{
-//         GET:(req,res)=>{
-//           res.writeHead(200,{'Content-Type': 'text/plain'});
-//           res.end('Hello world');  
-//         },
-//         POST: (req, res) => {
-//             let body = '';
-//             req.on('data', (chunk) => {
-//                 body += chunk;
-//             });
-//             req.on('end', () => {
-//                 res.writeHead(200, { 'Content-Type': 'text/plain' });
-//                 res.end(`POST request body: ${body}`);
-//             });
-//         },
-//         DELETE: (req, res) => {
-//             res.writeHead(200, { 'Content-Type': 'text/plain' });
-//             res.end('DELETE request');
-//         },
-//         PUT: (req, res) => {
-//             let body = '';
-//             req.on('data', (chunk) => {
-//                 body += chunk;
-//             });
-//             req.on('end', () => {
-//                 res.writeHead(200, { 'Content-Type': 'text/plain' });
-//                 res.end(`PUT request body: ${body}`);
-//             });
-//     },
-    
-// },
-// '/user':{
-//     GET: (req, res) => {
-//         res.writeHead(200,{'Content-Type':'text/plain'});
-//         res.end('get user')
-//     },
-//     POST: (req, res) => {
-//         res.writeHead(200,{'Content-Type':'textplain'})
-//         res.end('create user')
-//     },
-//     DELETE: (req, res) => {
-//         res.writeHead(200,{'Content-Type':'text/plain'})
-//         res.end('delete user')
-//     },
-//     PUT:(req, res) => {
-//         res.writeHead(200,{'Content-Type':'text/plain'})
-//         res.end('put user')
-//     }
-// }
-
-// }
-// const server = http.createServer((req,res)=>{
-//     const {method,url} = req;
-
-//     const handler = router[url]?.[method];
-//     if(!handler){
-//         res.writeHead(404,{'Content-Type':'text/plain'})
-//         res.end('404 Not Found')
-//     }
-// })
-// server.listen(port,()=>{
-//     console.log(` server is listening on ${port}`);
-// })
 
 const http = require("http");
 const port = 5000;
 
 const router = {
-  "GET": {},
-  "POST": {},
-  "PUT": {},
-  "DELETE": {}
+  get: {},
+  post: {},
+  put: {},
+  delete: {}
 };
 
 const server = http.createServer((req, res) => {
   const { method, url } = req;
-
-  if (router[method][url]) {
-    router[method][url](req, res);
+  if (router[method.toUpperCase()][url]) {
+    router[method.toUpperCase()][url](req, res);
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
@@ -88,12 +21,12 @@ const server = http.createServer((req, res) => {
 });
 
 // Add  GET routes 
-router.GET["/"] = (req, res) => {
+router.delete["/"] = (_req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello, world!');
 }
 //POST route
-router.POST["/"] = (req, res) => {
+router.post["/"] = (req, res) => {
   let body = '';
   req.on('data', (chunk) => {
     body += chunk;
@@ -104,7 +37,7 @@ router.POST["/"] = (req, res) => {
   })
 }
 //PUT route
-router.PUT["/"] = (req, res) => {
+router.put["/"] = (req, res) => {
     let body = '';
     req.on('data', (chunk) => {
       body += chunk;
@@ -116,10 +49,11 @@ router.PUT["/"] = (req, res) => {
   }
   
   //  DELETE route
-  router.DELETE["/"] = (req, res) => {
+  router.delete["/"] = (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Deleted Piece of Content!');
   }
 server.listen(port, () => {
   console.log(`server is listening on ${port}`);
 })
+
