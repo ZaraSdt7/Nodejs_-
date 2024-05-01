bba82310ed83e314641909b7e0f7a8a8b736a3df
zahra <zahra.st7373@gmail.com>
2024-05-01 10:17:05 +0330

    practice

--- functions-1.js
+++ functions-1.js
@@ -8,3 +8,16 @@ console.log(typeof a);//undefined
 console.log(typeof b);//number
 ////////////////////////////////
 
+//زوج یا فرد بودن اعداد
+
+console.log("pls insert ypur number:")
+let getnumber=10
+if(getnumber %2 == 0){
+    console.log(`number ${getnumber} is even`);
+}else{
+    console.log( `number ${getnumber} is odd`);
+}
+////////////////////////////////
+//function
+
+
---
+++ practice.js
@@ -0,0 +1,20 @@
+let avarage ="11";
+switch(avarage){
+    case "20":
+        case "19":
+            case "18":
+                console.log("Level A")
+                break;
+                case "17":
+                    case "16":
+                        case "15":
+                            console.log("Level B")
+                            break;
+                            case "14":
+                                case "13":
+                                    case "12":
+                                        console.log("Level C")
+                                        break;
+                                        default:
+                                            console.log("Level fired")
+}
